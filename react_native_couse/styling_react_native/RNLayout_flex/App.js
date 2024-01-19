3ad4880f7217638b74645759a85f450eaf4ac364
Mick <mac5y4@talktalk.net>
2024-01-19 18:23:17 +0000

    flex alignItem demmo

--- react_native_couse/styling_react_native/RNLayout_flex/App.js
+++ react_native_couse/styling_react_native/RNLayout_flex/App.js
@@ -3,27 +3,34 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00"} }>Box1</Box>
-      <Box style={{ backgroundColor: "#b65d1f"}}>Box2</Box>
+      <Box style={{ backgroundColor: "#8e9b00", paddingVertical: 100 }}>
+        Box1
+      </Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
-      {/* <Box style={{ backgroundColor: "#ab9156" ,flex: 4}}>Box4</Box>
-      <Box style={{ backgroundColor: "#6b0803" ,flex: 1}}>Box5</Box>
-      <Box style={{ backgroundColor: "#1c4c56" ,flex: 1}}>Box6</Box>
-      <Box style={{ backgroundColor: "#b95f21",flex: 1 }}>Box7</Box> */}
+      {/* <Box style={{ backgroundColor: "#ab9156", flex: 4 }}>Box4</Box>
+      <Box style={{ backgroundColor: "#6b0803", flex: 1 }}>Box5</Box>
+      <Box style={{ backgroundColor: "#1c4c56", flex: 1 }}>Box6</Box>
+      <Box style={{ backgroundColor: "#b95f21", flex: 1 }}>Box7</Box> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+   // flex: 1,
     flexDirection: "row",
-    justifyContent: "flex-start",
+    // justifyContent: "flex-start",
     // justifyContent: "flex-end",
     // justifyContent: "center",
     // justifyContent: "space-between",
     // justifyContent: "space-around",
     // justifyContent: "space-evenly",
+    // alignItems:"stretch",// defalt
+    // alignItems:"flex-start",
+    // alignItems:"flex-end",
+    // alignItems:"center",
+    alignItems: "baseline",
     marginTop: 64,
     borderWidth: 20,
     borderColor: "blue",
