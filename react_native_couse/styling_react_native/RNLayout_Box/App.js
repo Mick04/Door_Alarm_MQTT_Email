c0b2fc344d3ec43eb9076b95268e95d21245cd49
Mick <mac5y4@talktalk.net>
2024-01-19 14:18:27 +0000

    RNLayout_Box finished

---
+++ react_native_couse/styling_react_native/RNLayout_Box/App.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box7</Box>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 64,
+    borderWidth: 20,
+    borderColor: "red",
+  },
+});
