733e91805ac3b7efb109d36aedf3031b6870ada3
Mick <mac5y4@talktalk.net>
2024-01-20 12:56:07 +0000

    flex alignSelf demmo

--- react_native_couse/styling_react_native/RNLayout_flex/App.js
+++ react_native_couse/styling_react_native/RNLayout_flex/App.js
@@ -3,34 +3,32 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", paddingVertical: 200 }}>
+      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>
         Box1
       </Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box2</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
-      {/* <Box style={{ backgroundColor: "#ab9156", flex: 4 }}>Box4</Box>
-      <Box style={{ backgroundColor: "#6b0803", flex: 1 }}>Box5</Box>
-      <Box style={{ backgroundColor: "#1c4c56", flex: 1 }}>Box6</Box>
-      <Box style={{ backgroundColor: "#b95f21", flex: 1 }}>Box7</Box> */}
+      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>
+        Box2
+      </Box>
+      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center" }}>
+        Box3
+      </Box>
+      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
+        Box4
+      </Box>
+      {/* if no alignSelf is used it inerits the property from the parent
+       if there's no alignItem asigned to the parent then the defalt
+       stretch is is used */}
+      <Box style={{ backgroundColor: "#6b0803" }}>Box5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-   // flex: 1,
-    flexDirection: "row",
-    // justifyContent: "flex-start",
-    // justifyContent: "flex-end",
-    // justifyContent: "center",
-    // justifyContent: "space-between",
-    // justifyContent: "space-around",
-    // justifyContent: "space-evenly",
-    // alignItems:"stretch",// defalt
-    // alignItems:"flex-start",
-    // alignItems:"flex-end",
-    // alignItems:"center",
-    alignItems: "baseline",
+    flex: 1,
+    alignItems: "flex-start",
     marginTop: 64,
     borderWidth: 20,
     borderColor: "green",
