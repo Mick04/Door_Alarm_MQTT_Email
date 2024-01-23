79fe741751c6e0a12e1cad70f950257f34e74fd3
Mick <mac5y4@talktalk.net>
2024-01-23 19:14:09 +0000

    flex-shrink shrink box 1 brings both box's into the container but differ sizes  demo

--- react_native_couse/styling_react_native/RNLayout_flex/App.js
+++ react_native_couse/styling_react_native/RNLayout_flex/App.js
@@ -3,8 +3,12 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1 shrink</Box>
-      <Box style={{ backgroundColor: "#b65d1f", flexShrink: 1 }}>Box 2 shrink</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>
+        Box 1 shrink
+      </Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>
+        Box 2 shrink
+      </Box>
       {/* <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
       <Box style={{ backgroundColor: "#ab9156" }}>Box4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box5</Box>
@@ -16,9 +20,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-     flex: 1,
-    flexDirection:"row",
-     alignItems: "flex-start",
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
     width: 290,
     marginTop: 64,
     borderWidth: 6,
