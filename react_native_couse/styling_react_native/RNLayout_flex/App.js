83cb022f4124e71ecd51b5f71403e52b75f07b0a
Mick <mac5y4@talktalk.net>
2024-01-23 18:59:50 +0000

    flex-shrink demo settup

--- react_native_couse/styling_react_native/RNLayout_flex/App.js
+++ react_native_couse/styling_react_native/RNLayout_flex/App.js
@@ -3,25 +3,23 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box2</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1 shrink</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2 shrink</Box>
+      {/* <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
       <Box style={{ backgroundColor: "#ab9156" }}>Box4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box7</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box7</Box> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    flexWrap: "wrap",
-    // rowGap:20,
-    // columnGap:30,
-    gap: 20,
-    height: 300,
+     flex: 1,
+    flexDirection:"row",
+     alignItems: "flex-start",
+    width: 290,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
