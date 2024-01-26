22cfa6abcf1b607828e2b794dc10953a0b43be26
Mick <mac5y4@talktalk.net>
2024-01-26 16:30:04 +0000

    Dynamic user interfaceuse useWindowDimensions().width/hight to respond to rotation demo

---
+++ react_native_couse/Dynamic_user_interfaces/DinamicUI/App.js
@@ -0,0 +1,38 @@
+import { StyleSheet, View, Text, useWindowDimensions } from "react-native";
+
+export default function App() {
+  const windowWidth = useWindowDimensions().width;
+  const windowHight = useWindowDimensions().height;
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{fontSize: windowWidth > 500 ? 50 : 24,}}>Wellcome!</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- react_native_couse/Dynamic_user_interfaces/DynamicUI/App.js
+++ react_native_couse/Dynamic_user_interfaces/DynamicUI/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.text}>Wellcomennnnmmmmn!</Text>
+      </View>
     </View>
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: windowWidth > 500 ? "70%" : "90%",
+    height: windowHight > 600 ? "60%" : "90%",
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
