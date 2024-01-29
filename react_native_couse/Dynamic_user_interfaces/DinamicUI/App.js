e697fb775ec6d8175c96c83e963d93f3f71b6488
Mick <mac5y4@talktalk.net>
2024-01-29 14:27:58 +0000

    Dynamic user interface Plaform Specific Code Custom folders demo

--- react_native_couse/Dynamic_user_interfaces/DinamicUI/App.js
+++ react_native_couse/Dynamic_user_interfaces/DinamicUI/App.js
@@ -1,11 +1,24 @@
-import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  useWindowDimensions,
+} from "react-native";
+
+import CustomButton from "./components/CustomButton/CustomButton";
 
 export default function App() {
+  const { width, height } = useWindowDimensions();
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
         <View style={styles.box}>
-          <Text style={styles.text}>Wellcome!</Text>
+          <Text style={[styles.text, { fontSize: width > 500 ? 50 : 24 }]}>
+            Welcome
+          </Text>
+          <CustomButton title="Press Me" onPress={() => alert("Pressed!")} />
         </View>
       </View>
     </SafeAreaView>
@@ -16,15 +29,29 @@ const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
     backgroundColor: "plum",
+    paddingTop: Platform.OS === "android" ? 45 : 0,
   },
   container: {
     flex: 1,
     backgroundColor: "plum",
   },
   box: {
-   padding: 20,
+    flex: 1,
+    padding: 20,
   },
   text: {
-    fontSize: 24,
+    ...Platform.select({
+      ios: {
+        color: "purple",
+        fontWeight: "bold",
+      },
+      android: {
+        color: "blue",
+        fontStyle: "italic",
+        textAlign: "center",
+      },
+    }),
+
+    textAlign: "center",
   },
 });
