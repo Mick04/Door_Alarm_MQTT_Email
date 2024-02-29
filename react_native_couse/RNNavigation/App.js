9f8bad888790717d85c3f3327c5f9d9feaf70792
Mick <mac5y4@talktalk.net>
2024-02-29 13:03:39 +0000

    React Native navigtion linking to other screens, makes a good teplate for my tortoise heater

--- react_native_couse/RNNavigation/App.js
+++ react_native_couse/RNNavigation/App.js
@@ -12,15 +12,33 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
+        <Stack.Screen name="Home" component={HomeScreen}
+         options={{
+          title:"Home", // this is the header title
+       headerStyle:{ backgroundColor: "red"},
+       headerTintColor:"#fff",
+       headerTitleStyle:{fontWeight:"bold"}
+        }} />
+        <Stack.Screen 
           name="About"
           component={AboutScreen}
+          options={{
+          title:"About", // this is the header title
+       headerStyle:{ backgroundColor: "#6c54ba"},
+       headerTintColor:"#fff",
+       headerTitleStyle:{fontWeight:"bold"}
+        }}
           initialParams={{ name: "guest" }}
         />
         <Stack.Screen
           name="Heater"
-          component={HeaterScreen}
+          component={HeaterScreen}   
+          options={{
+          title:"Tortoise Heater", // this is the header title
+       headerStyle:{ backgroundColor: "#FcF4aa"},
+       headerTintColor:"#fff",
+       headerTitleStyle:{fontWeight:"bold",color:"black"}
+        }}
           initialParams={{ name: "Mick" }}
         />
       </Stack.Navigator>
